bf80c464a115f0b1567b9f85c706ca1a66a14862
Methos02 <methos_02@gmx.fr>
2019-10-06 21:44:46 +0200

    ajout disabled html_input

--- src/php/html_input/input.php
+++ src/php/html_input/input.php
@@ -1,5 +1,5 @@
 <?php if(!isset($params) || !is_array($params)) { exit;} ?>
-<label class="label-compact<?= $params['width'] . $params['class_before'] ?>" <?= $params['view'] ?>>
+<label class="label-compact<?= $params['width'] . $params['class_before'] ?>" <?= $params['view'] . $params['disabled'] ?>>
     <input type="<?= $params['type'] ?>" name="<?= $params['nom'] ?>" class="input-compact<?= $params['error'] != ""? ' input_erreur' : '' ?>" data-type="<?= $params['dataType'] ?>" <?= $params['valueFormat'] . $params['obliger'] . $params['message'] ?> >    <span class="label-input" <?= $params['error'] != ""? 'style="display:none"' : '' ?> > <?= $params['label'] ?></span>
     <span class="label-input" <?= $params['error'] != ""? 'style="display:none"' : '' ?> > <?= $params['label'] ?></span>
     <?= $params['error'] ?>
