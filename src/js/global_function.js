42aa3ead04fb65837beed4655c7c231d0b33d351
Methos02 <methos_02@gmx.fr>
2019-10-06 21:57:41 +0200

    Ajout function insertErreur, hydrateForm et defineParamInput dans fonction global

--- src/js/global_function.js
+++ src/js/global_function.js
@@ -1,9 +1,15 @@
 import {cleanForm} from "./tools/formTool/cleanForm";
 import {resetFile} from "./tools/formTool/resetFile";
 import {external_init} from "./tools/init";
+import {insertErreur} from "./tools/insertErreur";
+import {hydrateForm} from "./tools/formTool/hydrateForm";
+import {defineParamInput} from "./tools/defineParamInput";
 
 export let FormCustom = {
     cleanForm: cleanForm,
     resetFile : resetFile,
-    external_init : external_init
+    external_init : external_init,
+    insertErreur : insertErreur,
+    hydraForm : hydrateForm,
+    defineParamInput : defineParamInput
 };
