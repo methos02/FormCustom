7bedf64393bfa3f81e3781d69bdefaaccccca4be
Methos02 <methos_02@gmx.fr>
2021-07-22 11:01:10 +0200

    - suppresion des erreurs et du div label des inputs hidens

--- src/php/html_input/hidden.php
+++ src/php/html_input/hidden.php
@@ -1,5 +1,2 @@
 <?php if(!isset($params) || !is_array($params)) { exit;} ?>
-<label class="label-compact">
-    <input type="hidden" name="<?= $params['nom'] ?>" data-type="<?= $params['dataType'] ?>" <?= $params['valueFormat'] . $params['obliger'] . $params['message'] ?> >
-    <?= $params['error'] ?>
-</label>
+<input type="hidden" name="<?= $params['nom'] ?>" data-type="<?= $params['dataType'] ?>" <?= $params['valueFormat'] . $params['obliger'] . $params['message'] ?> >
